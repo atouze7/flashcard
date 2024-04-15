2b8c2fc562cece5dc509cb1af087471f317e60be
atouze7 <atouze7@gmail.com>
2024-04-15 16:42:28 -0400

    1st commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+//import { Button } from 'react-materialize'
+import { withRouter } from "react-router-dom";
+
+export default class Home extends Component {
+  render() {
+    const Button1 = withRouter(({ history }) => (
+      <button
+        type="button"
+        className="button btn-large red"
+        waves="light"
+        onClick={() => {
+          history.push("/projects");
+        }}
+      >
+        Study
+      </button>
+    ));
+
+    const Button2 = withRouter(({ history }) => (
+      <button
+        type="button"
+        className="button btn-large"
+        waves="light"
+        onClick={() => {
+          history.push("/addproject");
+        }}
+      >
+        Create New
+      </button>
+    ));
+
+    return (
+      <div className="App-header">
+        <h1 id="header-text">Welcome To</h1>
+        <div className="header-container">
+          <h2 id="header-text2">BrainBurst!</h2>
+        </div>
+        <div className="button-container">
+          <Button1 />
+          <Button2 />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://atouze7:ballislife2724@cluster0.kc9qlmn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+};
